77a60a78a089f5d6fff07f906d8a993f52ef9b3f
Austen <39606633+kvoli@users.noreply.github.com>
2020-04-28 20:31:14 +1000

    update with converter & points for map (#20)

--- src/client/src/js/components/MapBox.jsx
+++ src/client/src/js/components/MapBox.jsx
@@ -3,7 +3,10 @@ import mapboxgl from 'mapbox-gl'
 import {MAPBOX_KEY, MAPBOX_PUB_KEY} from '../constants/config'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import * as melb_geo from '../constants/melbourne.geojson'
+import * as melb_points from '../constants/sampleTwitter.json'
 import { Container } from '@material-ui/core'
+import  { jsonGeoJson } from '../helpers/index'
+import Sidebar from './Sidebar'
 
 const styles = {
   width: "100vw",
@@ -17,6 +20,10 @@ const MapBox = () => {
     const [lat, setLat] = useState(-37.84);
     const [zoom, setZoom] = useState(8);
     const [map, setMap] = useState(null);
+    const [sidebar, setSidebar] = useState(false);
+    const [selected, setSelected] = useState(null);
+
+    const points = jsonGeoJson(melb_points);
 
 
     useEffect(() => {
@@ -24,7 +31,7 @@ const MapBox = () => {
         const initializeMap = ({ setMap, mapContainer }) => {
           const map = new mapboxgl.Map({
             container: mapContainer.current,
-            style: "mapbox://styles/kvoli/ck9dizunz0vf51io892dqx4gw", 
+            style: "mapbox://styles/shijiel2/cjvcb640p3oag1gjufck6jcio", 
             center: [lng, lat],
             zoom: zoom
           });
@@ -37,17 +44,102 @@ const MapBox = () => {
               'data': melb_geo
             })
 
+            map.addSource('points', {
+              'type': 'geojson',
+              'data': points
+            })
+
             map.addLayer({
               'id': 'suburb-border',
               'type': 'line',
               'source': 'suburbs',
               "paint": {
-                "line-color": "#8B4225",
+                "line-color": "#22B0C0",
                 "line-width": 0.5,
                 "line-opacity": 1
               }
             })
 
+            map.addLayer({
+              'id': 'suburb-fills',
+              'type': 'fill',
+              'source': 'suburbs',
+              "paint": {
+                'fill-color': "#00ffff",
+                "fill-opacity": ["case",
+                  ["boolean", ["feature-state", "hover"], false],
+                  0.5,
+                  0.2
+                ]
+              }
+            });
+
+            map.addLayer({
+              id: 'test-heat',
+              type: 'heatmap',
+              source: 'points',
+              maxzoom: 15,
+              paint: {
+                // increase weight as diameter breast height increases
+                'heatmap-weight': 1,
+                // increase intensity as zoom level increases
+                'heatmap-intensity': {
+                  stops: [
+                    [11, 1],
+                    [15, 3]
+                  ]
+                },
+                // assign color values be applied to points depending on their density
+                'heatmap-color': [
+                  'interpolate',
+                  ['linear'],
+                  ['heatmap-density'],
+                  0, "rgba(33,102,172,0)",
+                  0.2, "rgb(103,169,207)",
+                  0.4, "rgb(209,229,240)",
+                  0.6, "rgb(253,219,199)",
+                  0.8, "rgb(239,138,98)",
+                  1, "rgb(178,24,43)"
+                ],
+                // increase radius as zoom increases
+                'heatmap-radius': {
+                  stops: [
+                    [11, 15],
+                    [15, 20]
+                  ]
+                },
+                // decrease opacity to transition into the circle layer
+                'heatmap-opacity': {
+                  default: 1,
+                  stops: [
+                    [5, 0.5],
+                    [14, 1],
+                    [15, 0]
+                  ]
+                },
+              }
+            });
+
+            map.addLayer({
+              id: 'test-point',
+              type: 'circle',
+              source: 'points',
+              minzoom: 14,
+              paint: {
+                // increase the radius of the circle as the zoom level and dbh value increases
+                'circle-radius': 7,
+                'circle-color': 'rgb(178,24,43)',
+                'circle-stroke-color': 'white',
+                'circle-stroke-width': 1,
+                'circle-opacity': {
+                  stops: [
+                    [14, 0],
+                    [15, 1]
+                  ]
+                }
+              }
+            });
+      
           });
         };
     
@@ -55,8 +147,11 @@ const MapBox = () => {
       }, [map]);
 
     return (
+      <>
         <div ref={el => (mapContainer.current = el)} style={styles} />
-      )
+        <Sidebar opened={sidebar} selected={selected}/>
+      </>
+    )
     
 }
 
---
+++ src/client/src/js/helpers/index.js
@@ -0,0 +1,33 @@
+export const jsonGeoJson = json => {
+
+    let res = {}
+    let features = []
+    let cur = {}
+    console.log(json)
+    res["type"] = "FeatureCollection"
+    json["default"]["rows"].forEach(element => {
+        cur["type"] = "Feature"
+        cur["properties"] = {
+            "harvestTime": element["harvestTime"],
+            "_id": element["_id"],
+            "harvestNode": element["harvestNode"],
+            "createdTime": element["created_at"],
+            "user": element["user"],
+            "hashtags": element["hashtags"],
+            "suburb": element["location"]["suburb"] ? element["location"]["suburb"] : "",
+            "text": element["text"]
+        }
+        cur["geometry"] = {
+            "type": "Point",
+            "coordinates": [element["location"]["longitude"], element["location"]["latitude"]]
+        }
+        features.push(cur)
+        cur = {}
+    });
+
+    res["features"] = features
+    
+    return res
+
+}
+
