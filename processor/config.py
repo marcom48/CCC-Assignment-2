a31359766a328cd688fea4290a01d22689a6043e
MarcoM <marco@marasco.id.au>
2020-05-04 10:13:05 +1000

    Update view string

--- processor/config.py
+++ processor/config.py
@@ -3,6 +3,6 @@ import os
 COUCHDB_USER = os.getenv("COUCHDB_USER")
 COUCHDB_PASSWORD = os.getenv("COUCHDB_PASSWORD")
 
-SUBURB_GET = "http://127.0.0.1:5984/located_tweets/_design/floattest/_view/floattest?group_level=4"
+SUBURB_GET = "http://127.0.0.1:5984/located_tweets/_design/test/_view/test?group_level=4"
 USER_GET = "http://127.0.0.1:5984/tweets/_design/floattest/_view/floattest?group_level=4"
 
